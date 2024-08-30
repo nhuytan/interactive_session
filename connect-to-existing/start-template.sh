6bc3ad377e889bb166fe84588f153478a6fcddab
avidalto <avidalto@hawk.iit.edu>
2024-08-30 03:34:53 -0500

    Add support for nginx wrapper

--- connect-to-existing/start-template.sh
+++ connect-to-existing/start-template.sh
@@ -1,8 +1,77 @@
 
+if [ -z "${service_existing_port}" ]; then
+    # Not using the nginx wrapper
+    
+    # Notify platform that service is running
+    ${sshusercontainer} "${pw_job_dir}/utils/notify.sh Running"
+
+    sleep infinity
+fi
+
+#######################
+# START NGINX WRAPPER #
+#######################
+
+# Initialize cancel script
+echo '#!/bin/bash' > cancel.sh
+chmod +x cancel.sh
+
+echo "Starting nginx wrapper on service port ${service_port}"
+
+# Write config file
+cat >> config.conf <<HERE
+server {
+ listen ${service_port};
+ server_name _;
+ index index.html index.htm index.php;
+ add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+ add_header 'Access-Control-Allow-Headers' 'Authorization,Content-Type,Accept,Origin,User-Agent,DNT,Cache-Control,X-Mx-ReqToken,Keep-Alive,X-Requested-With,If-Modified-Since';
+ add_header X-Frame-Options "ALLOWALL";
+ client_max_body_size 1000M;
+ location / {
+     proxy_pass http://127.0.0.1:${service_existing_port}/me/${openPort}/;
+     proxy_http_version 1.1;
+       proxy_set_header Upgrade \$http_upgrade;
+       proxy_set_header Connection "upgrade";
+       proxy_set_header X-Real-IP \$remote_addr;
+       proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+       proxy_set_header Host \$http_host;
+       proxy_set_header X-NginX-Proxy true;
+ }
+}
+HERE
+
+if [ -f "${service_nginx_sif}" ]; then
+    echo "Running singularity container ${service_nginx_sif}"
+    # We need to mount $PWD/tmp:/tmp because otherwise nginx writes the file /tmp/nginx.pid 
+    # and other users cannot use the node. Was not able to change this in the config.conf.
+    mkdir -p ./tmp
+    touch empty
+    singularity run -B $PWD/tmp:/tmp -B $PWD/config.conf:/etc/nginx/conf.d/config.conf -B empty:/etc/nginx/conf.d/default.conf ${service_nginx_sif} &
+    echo "kill $!" >> cancel.sh
+else
+    if ! sudo -n true 2>/dev/null; then
+        displayErrorMessage "ERROR: NGINX DOCKER CONTAINER CANNOT START PW BECAUSE USER ${USER} DOES NOT HAVE SUDO PRIVILEGES"
+    fi
+
+    container_name="nginx-${service_port}"
+    # Remove container when job is canceled
+    echo "sudo docker stop ${container_name}" >> cancel.sh
+    echo "sudo docker rm ${container_name}" >> cancel.sh
+    # Start container
+    sudo service docker start
+    touch empty
+    sudo docker run  -d --name ${container_name} \
+         -v $PWD/config.conf:/etc/nginx/conf.d/config.conf \
+         -v $PWD/empty:/etc/nginx/conf.d/default.conf \
+         --network=host nginxinc/nginx-unprivileged:1.25.3
+    # Print logs
+    sudo docker logs ${container_name}
+fi
+
+
+
 # Notify platform that service is running
 ${sshusercontainer} "${pw_job_dir}/utils/notify.sh Running"
 
-# If running docker with the -d option sleep here! 
-# Do not exit this script until the job is canceled!
-# Exiting this script before the job is canceled triggers the cancel script!
 sleep infinity
