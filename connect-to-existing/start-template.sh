414a795865a564006ab3d776d6d79d544859b3c7
avidalto <avidalto@hawk.iit.edu>
2024-08-30 02:43:33 -0500

    Add connect to existing server

---
+++ connect-to-existing/start-template.sh
@@ -0,0 +1,8 @@
+
+# Notify platform that service is running
+${sshusercontainer} "${pw_job_dir}/utils/notify.sh Running"
+
+# If running docker with the -d option sleep here! 
+# Do not exit this script until the job is canceled!
+# Exiting this script before the job is canceled triggers the cancel script!
+sleep inifinity
