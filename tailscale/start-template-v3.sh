4d7701db87fe3dab111b1f28a5d0881a06d2b3e2
avidalto <avidalto@hawk.iit.edu>
2025-02-27 10:11:32 -0600

    Add tailscale

---
+++ tailscale/start-template-v3.sh
@@ -0,0 +1,24 @@
+# Runs via ssh + sbatch
+set -x
+
+curl -fsSL https://tailscale.com/install.sh | sh
+
+# Prepare kill service script
+# - Needs to be here because we need the hostname of the compute node.
+# - kill-template.sh --> service-kill-${job_number}.sh --> service-kill-${job_number}-main.sh
+echo "Creating file ${resource_jobdir}/service-kill-${job_number}-main.sh from directory ${PWD}"
+if [[ ${jobschedulertype} != "CONTROLLER" ]]; then
+    # Remove .cluster.local for einteinmed!
+    hname=$(hostname | sed "s/.cluster.local//g")
+    echo "ssh ${hname} 'bash -s' < ${resource_jobdir}/service-kill-${job_number}-main.sh" > ${resource_jobdir}/service-kill-${job_number}.sh
+else
+    echo "bash ${resource_jobdir}/service-kill-${job_number}-main.sh" > ${resource_jobdir}/service-kill-${job_number}.sh
+fi
+
+cat >> ${resource_jobdir}/service-kill-${job_number}-main.sh <<HERE
+sudo tailscale down
+HERE
+
+sudo tailscale up | tee tailscale.up.log
+
+tailscale ip
